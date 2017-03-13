3f8cc243e47119dc5af5bc0992ffd26510789356
rohitshedage <rohitshedage@gmail.com>
2017-03-13 23:31:19 +0530

    Updated Model For Visit

--- app/models/visit.js
+++ app/models/visit.js
@@ -1,9 +1,21 @@
 var mongoose = require('mongoose');
 
+
+var visitSchema = new mongoose.Schema({
+	vehicleNumber: String,
+	visitingToFlat: String,
+	visitingPerson: String,
+	purposeOfVisit: String,
+	noOfVisitors: Number,
+	entryGate: String,
+	exitGate: String,
+	entryTime: String,
+	exitTime: String
+}); 
+
 module.exports = mongoose.model('Visit', {
 	name: String,
-	contactNumber: String,
-	identityType: String,
-	identiyNumber: String,
-	noOfVisitors: Number
+	contactNumber: {type: String, required: true, unique: true},
+	societyName: String,
+	visits:[visitSchema]
 });
