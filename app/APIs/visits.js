330572da80d8093145b31c0af7a1af638f06d108
rohitshedage <rohitshedage@gmail.com>
2017-03-15 09:37:32 +0530

    Updated post method for visits to have multiple visits

--- app/APIs/visits.js
+++ app/APIs/visits.js
@@ -1,29 +1,52 @@
 var Visit = require('../models/visit');
 
+
 module.exports.listAll = function (req, res){
 	Visit.find({}, function(err, results){
 		logError(err);
 		return res.json(results);
 	});
-}
+};
 
 module.exports.fetch = function (req, res){
-	Visit.find({contactNumber: req.params.contactNumber}, function(err, results){
-		logError(err);
-		return res.json(results);
-	});
-}
+	return fetchVisitsForGivenVisitor(req.params.contactNumber)
+		.then((visitors) => {
+			res.json(visitors);
+		});
+};
 
 module.exports.create = function (req, res){
-	var visit = new Visit(req.body);
-	visit.save(function(err, result){
-		logError(err);
-		return res.json(result);
-	});
-}
+
+	return fetchVisitsForGivenVisitor(req.body.contactNumber)
+		.then((visitor) => {
+			var visit;
+			if (!visitor || visitor.length === 0) {
+				visit = new Visit(req.body);
+			} else {
+				visit = visitor[0];
+				var currentVisit = new Visit(req.body);
+				visit.visits.push(currentVisit.visits[0]);
+			}
+			
+			visit.save(function(err, result){
+				logError(err);
+				return res.json(result);
+			});
+		});
+};
 
 function logError(err) {
-	if(err){
-		console.log(err);
-	}
+	console.log(err);
+}
+
+function fetchVisitsForGivenVisitor(contactNumber){
+	return new Promise((resolve, reject) => {
+		Visit.find({contactNumber: contactNumber}, function(err, results){
+			if (err) {
+				logError(err);
+				return reject(err);
+			}			
+			return resolve(results);
+		});	
+	});
 }
